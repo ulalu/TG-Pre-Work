90b641029fa287a403b869cb59a810b78b1b23a5
Ula MV <uvanderlaan@gmail.com>
2018-07-24 22:58:39 -0600

    Ex 6

--- JSprework/Introduction/Variables/app.js
+++ JSprework/Introduction/Variables/app.js
@@ -1,12 +1,2 @@
-let molecule = 16;
-let particle = 18;
-let assay = 3;
-
-// Add and assign to molecule below
-molecule += 16;
-
-// Multiply and assign to particle below
-particle *= 6.02;
-
-// Increment assay by 1
-assay ++;
+let favoriteAnimal = "orca";
+console.log("My favorite animal:" + favoriteAnimal)
