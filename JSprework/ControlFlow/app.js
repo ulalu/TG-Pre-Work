bf24aede2b9f0f3b37b2d4e2d6087774110ccc95
Ula MV <uvanderlaan@gmail.com>
2018-07-25 12:07:51 -0600

    Ex 10

--- JSprework/ControlFlow/app.js
+++ JSprework/ControlFlow/app.js
@@ -1,17 +1,17 @@
-let moonPhase = 'full';
+let isLocked = false;
+
+isLocked ? console.log('You will need a key to open the door.') : console.log('You will not need a key to open the door.');
 
 
-switch (moonPhase) {
-  case 'full':
-    console.log('Howl!');
-    break;
-  case 'mostly full':
-    console.log('Arms and legs are getting hairier');
-    break;
-  case 'mostly new':
-  	console.log('Back on two feet');
-    break;
-  default:
-    console.log('Invalid moon phase');
-  	break;
+let isCorrect = true;
+
+isCorrect ? console.log('Correct!') : console.log('Incorrect!');
+
+
+let favoritePhrase = 'Love That!';
+
+favoritePhrase ? 'Love That!') {
+  console.log('I love that!');
+} else {
+  console.log("I don't love that!");
 }
