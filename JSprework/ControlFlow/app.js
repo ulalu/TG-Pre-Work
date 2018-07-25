ce2c1ab6f41ca110625e6364c8153164c4376f4f
Ula MV <uvanderlaan@gmail.com>
2018-07-25 11:44:28 -0600

    Ex 4

--- JSprework/ControlFlow/app.js
+++ JSprework/ControlFlow/app.js
@@ -9,7 +9,7 @@ if (wordCount) {
 
 let favoritePhrase = 'The time has come the walrus said.';
 
-if (favoritePhrase) {
+if (!favoritePhrase) {
   console.log("This string doesn't seem to be empty.");
 } else {
   console.log('This string is definitely empty.');
