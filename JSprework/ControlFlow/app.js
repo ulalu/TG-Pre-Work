86088073b854a06a5ae5a06d05aa6386a657f7f4
Ula MV <uvanderlaan@gmail.com>
2018-07-25 12:01:55 -0600

    Ex 9

--- JSprework/ControlFlow/app.js
+++ JSprework/ControlFlow/app.js
@@ -1,12 +1,17 @@
 let moonPhase = 'full';
-let isFoggyNight = false;
 
-if (moonPhase === 'full' || isFoggyNight) {
-  console.log('Howl!')
-} else if (moonPhase === 'mostly full') {
-  console.log('Arms and legs are getting hairier')
-} else if (moonPhase === 'mostly new') {
-  console.log('Back on two feet')
-} else {
-  console.log('Invalid moon phase')
+
+switch (moonPhase) {
+  case 'full':
+    console.log('Howl!');
+    break;
+  case 'mostly full':
+    console.log('Arms and legs are getting hairier');
+    break;
+  case 'mostly new':
+  	console.log('Back on two feet');
+    break;
+  default:
+    console.log('Invalid moon phase');
+  	break;
 }
