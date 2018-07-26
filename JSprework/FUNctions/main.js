5f6fe453a6453b504a42411f6d8578e87e97e668
Ula MV <uvanderlaan@gmail.com>
2018-07-25 19:01:35 -0600

    Ex 4

--- JSprework/FUNctions/main.js
+++ JSprework/FUNctions/main.js
@@ -1,5 +1,7 @@
-const takeOrder = (topping) => {
-  console.log('Order: pizza topped with ' + topping);
+const takeOrder = (topping, crustType) => {
+  console.log('Order: ' + crustType + 'pizza topped with ' + topping);
 };
 
-takeOrder('black olives');
+takeOrder('mushrooms', 'cracker crust');
+takeOrder('olives', 'thin crust');
+takeOrder('pepperoni', 'stuffed crust');
