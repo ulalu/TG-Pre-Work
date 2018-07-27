0fefd1ac252db165531fe19c660ed8ab71279ca1
Ula MV <uvanderlaan@gmail.com>
2018-07-27 14:46:22 -0600

    Ex 4

--- JSprework/Arrays/main.js
+++ JSprework/Arrays/main.js
@@ -1,5 +1,6 @@
 let newYearsResolutions = ['Exercize More','Build a Web App','Plant a Food Garden']
 
+newYearsResolutions[1] = 'Learn a new language';
 let listItem = newYearsResolutions[0]
 
 console.log(newYearsResolutions[2])
