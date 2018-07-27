d77b2eb0105c4d26eceec88171d9c9f7dbd82e18
Ula MV <uvanderlaan@gmail.com>
2018-07-27 14:50:21 -0600

    Ex 6

--- JSprework/Arrays/main.js
+++ JSprework/Arrays/main.js
@@ -6,3 +6,8 @@ let listItem = newYearsResolutions[0]
 console.log(newYearsResolutions[2])
 console.log(newYearsResolutions[3])
 console.log(newYearsResolutions.length)
+
+newYearsResolutions.push('Learn how to keep bees', 'Save to buy a car in cash')
+console.log(newYearsResolutions)
+
+newYearsResolutions.pop()
