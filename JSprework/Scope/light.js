36fe769efc737f741a85f4dffc9b9e2b9b456150
Ula MV <uvanderlaan@gmail.com>
2018-07-27 11:16:59 -0600

    Ex 5

--- JSprework/Scope/light.js
+++ JSprework/Scope/light.js
@@ -1,8 +1,12 @@
 const visibleLightWaves = () => {
   let lightWaves = 'Moonlight';
   console.log(lightWaves);
+  let region = 'The Arctic';
+  if (region === 'The Arctic') {
+    let lightWaves = 'Northern Lights'
+    console.log(lightWaves)
+  }
 };
 
 
 visibleLightWaves();
-console.log(lightWaves);
