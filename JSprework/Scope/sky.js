09e03fa9fedd493eceed32dd57f9fd81c372f516
Ula MV <uvanderlaan@gmail.com>
2018-07-27 11:08:23 -0600

    Ex 3

--- JSprework/Scope/sky.js
+++ JSprework/Scope/sky.js
@@ -4,7 +4,9 @@ const galaxy = 'The Milky Way'
 let stars = 'North Star'
 
 const myNightSky = () => {
+  stars = 'Sirius';
   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 }
 
 console.log(myNightSky())
+console.log(stars)
