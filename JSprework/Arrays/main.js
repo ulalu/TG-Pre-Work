3ee5dd38beaf41f0bc3cbc034c464a1c365a62ed
Ula MV <uvanderlaan@gmail.com>
2018-07-27 11:42:54 -0600

    Ex 3

--- JSprework/Arrays/main.js
+++ JSprework/Arrays/main.js
@@ -1,3 +1,6 @@
 let newYearsResolutions = ['Exercize More','Build a Web App','Plant a Food Garden']
 
-console.log(newYearsResolutions)
+let listItem = newYearsResolutions[0]
+
+console.log(newYearsResolutions[2])
+console.log(newYearsResolutions[3])
