867f1f007c6ca3001d78b61747dec407702e7e09
Ula MV <uvanderlaan@gmail.com>
2018-07-28 15:47:00 -0600

    Ex 4

--- JSprework/Iterators/main.js
+++ JSprework/Iterators/main.js
@@ -1,13 +1,10 @@
-let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
+let randomNumbers = [375, 200, 3.14, 7, 13, 852];
 
-// Create the secretMessage array below
-let secretMessage = animals.map(animal => animal[0]);
+// Call .filter() on randomNumbers below
+let smallNumbers = randomNumbers.filter(number => number < 250);
 
-console.log(secretMessage.join(''));
+let favoriteWords = ['nostalgia', 'hyperbole', 'fervent', 'esoteric', 'serene'];
 
-let bigNumbers = [100, 200, 300, 400, 500];
+let longFavoriteWords = favoriteWords.filter(word => word.length > 7);
 
-// Create the smallNumbers array below
-let smallNumbers = bigNumbers.map(function(number){
-  return number / 100
-});
+// Refactor the code above using arrow function syntax
