71d9f372cdcbca19ce5df68f749f1a879d170119
Ula MV <uvanderlaan@gmail.com>
2018-07-28 17:20:17 -0600

    Ex 12

--- JSprework/Objects/main.js
+++ JSprework/Objects/main.js
@@ -1,34 +1,12 @@
 let person = {
-  name: 'Ursula',
-  age: 27,
-  weekendAlarm: 'No alarms needed',
-  weekAlarm: 'Alarm set to 7AM',
-  sayHello: function(){return `Hello, myname is ${this.name}.`},
-  sayGoodbye() {return 'Goodbye!'}
-}
+  _name: 'Lu Xun',
+  _age: 137,
 
-let friend = {
-  name: 'Carmen',
-}
-
-friend.sayHello = person.sayHello
-
-let day = 'Saturday';
-
-let alarm;
-
-if (day === 'Saturday' || day === 'Sunday') {
-  alarm = 'weekendAlarm';
-} else {
-  alarm = 'weekAlarm';
+set age(newAge) {
+  if (typeof newAge === 'number') {
+    this._age = newAge;
+  } else {
+    return console.log('Invalid input')
   }
-
-console.log(person['name']);
-console.log(person['age']);
-console.log(person[alarm]);
-
-person.hobbies = ['Sewing','Painting']
-person.hobbies = ['Cycling']
-console.log(person['hobbies'])
-console.log(person.sayHello())
-console.log(friend.sayHello())
+}
+};
