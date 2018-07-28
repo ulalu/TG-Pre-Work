7693fff9bd50991c9e21618a6a93f113651bb757
Ula MV <uvanderlaan@gmail.com>
2018-07-27 22:53:45 -0600

    Ex 4

--- JSprework/Loops/main.js
+++ JSprework/Loops/main.js
@@ -1,5 +1,5 @@
 let vacationSpots = ['Mumbai', 'Capetown', 'Florence']
 
-for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++) {
+for (let vacationSpotIndex = vacationSpots.length-1; vacationSpotIndex >= 0; vacationSpotIndex--) {
   console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
 }
