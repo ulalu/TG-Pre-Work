39e051450a845f73825a69872ead97369250cbf4
Ula MV <uvanderlaan@gmail.com>
2018-07-28 16:51:36 -0600

    Ex 5

--- JSprework/Objects/main.js
+++ JSprework/Objects/main.js
@@ -1,7 +1,20 @@
 let person = {
   name: 'Ursula',
   age: 27,
+  weekendAlarm: 'No alarms needed',
+  weekAlarm: 'Alarm set to 7AM'
 }
+
+let day = 'Saturday';
+
+let alarm;
+
+if (day === 'Saturday' || day === 'Sunday') {
+  alarm = 'weekendAlarm';
+} else {
+  alarm = 'weekAlarm';
+  }
 
 console.log(person['name']);
 console.log(person['age']);
+console.log(person[alarm]);
