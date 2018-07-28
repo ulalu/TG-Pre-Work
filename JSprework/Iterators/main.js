97281298866a041bc64bf822b4462800cc7fab47
Ula MV <uvanderlaan@gmail.com>
2018-07-28 15:44:47 -0600

    Ex 3

--- JSprework/Iterators/main.js
+++ JSprework/Iterators/main.js
@@ -1,4 +1,13 @@
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-// Iterate over fruits below
-fruits.forEach(fruit => console.log('I want to eat a ' + fruit));
+// Create the secretMessage array below
+let secretMessage = animals.map(animal => animal[0]);
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(function(number){
+  return number / 100
+});
