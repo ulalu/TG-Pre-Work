85a0b19fae0b74bb8a1d28b85f3adadf539266c2
Ula MV <uvanderlaan@gmail.com>
2018-07-31 17:16:33 -0600

    Ex 4

--- JSprework/BrowserCompatibility/main.js
+++ JSprework/BrowserCompatibility/main.js
@@ -1,5 +1,8 @@
-// Set the variable below to a number
-let esFivePercentageSupport = 96.85;
+var pasta = "Spaghetti"; // ES5 syntax
 
-// Set the variable below to a number
-let esSixTemplateLiterals = 88.72;
+var meat = "Pancetta"; // ES6 syntax
+
+var sauce = "Eggs and cheese"; // ES6 syntax
+
+// Template literals, like the one below, were introduced in ES6
+var carbonara = 'You can make carbonara with ' + pasta + ', ' + meat + '  and a sauce made with ' + sauce + '.';
