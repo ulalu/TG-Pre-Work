5b2cfb17932723332ac9c10f768061e99801b619
Ula MV <uvanderlaan@gmail.com>
2018-07-31 17:13:43 -0600

    Ex 3

--- JSprework/BrowserCompatibility/main.js
+++ JSprework/BrowserCompatibility/main.js
@@ -1,8 +1,5 @@
-var pasta = "Spaghetti"; // ES5 syntax
+// Set the variable below to a number
+let esFivePercentageSupport = 96.85;
 
-const meat = "Pancetta"; // ES6 syntax
-
-let sauce = "Eggs and cheese"; // ES6 syntax
-
-// Template literals, like the one below, were introduced in ES6
-const carbonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
+// Set the variable below to a number
+let esSixTemplateLiterals = 88.72;
