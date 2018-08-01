a33764a376dcd98ebc55e63fc3c1defb99547626
Ula MV <uvanderlaan@gmail.com>
2018-07-31 23:16:42 -0600

    Ex 13

--- JSprework/Modules/airplane.js
+++ JSprework/Modules/airplane.js
@@ -1,24 +1,24 @@
-let availableAirplanes = [{
- name: 'AeroJet',
+export let availableAirplanes = [
+{name: 'AeroJet',
  fuelCapacity: 800,
  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
-  maxSpeed: 1200,
-  minSpeed: 300
+ maxSpeed: 1200,
+ minSpeed: 300
 },
 {name: 'SkyJet',
  fuelCapacity: 500,
  availableStaff: ['pilots', 'flightAttendants'],
  maxSpeed: 800,
  minSpeed: 200
+}
+];
 
-}];
-
-let flightRequirements = {
+export let flightRequirements = {
   requiredStaff: 4,
   requiredSpeedRange: 700
 };
 
-function meetsStaffRequirements(availableStaff, requiredStaff) {
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
   if (availableStaff.length >= requiredStaff) {
     return true;
   } else {
@@ -26,13 +26,13 @@ function meetsStaffRequirements(availableStaff, requiredStaff) {
   }
 };
 
-function meetsSpeedRangeRequirements(maxSpeed,minSpeed,requiredSpeedRange){
+function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
   let range = maxSpeed - minSpeed;
   if (range > requiredSpeedRange) {
     return true;
-  } else {
+    } else {
     return false;
   }
 };
 
-export{availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs};
+export default meetsSpeedRangeRequirements;
