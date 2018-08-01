170fee22d74096cce259fbd03e011a68a90e6517
Ula MV <uvanderlaan@gmail.com>
2018-07-31 23:02:21 -0600

    Ex 9

--- JSprework/Modules/airplane.js
+++ JSprework/Modules/airplane.js
@@ -1,28 +1,36 @@
+export let availableAirplanes = [{
+ name: 'AeroJet',
+ fuelCapacity: 800,
+ availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+  maxSpeed: 1200,
+  minSpeed: 300
+},
+{name: 'SkyJet',
+ fuelCapacity: 500,
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
 
+}];
 
-
-let availableAirplanes = [
-{
-  name: 'AeroJet',
-  fuelCapacity: 800,
-  availableStaff: ['pilots','flightAttendants','engineers','medicalAssistance', 'sensorOperators']
- },
- {name: 'SkyJet',
-  fuelCapacity: 500,
-  availableStaff: ['pilots', 'flightAttendants']
- }
-];
-
-let flightRequirements = {
-	requiredStaff: 4
+export let flightRequirements = {
+  requiredStaff: 4,
+  requiredSpeedRange: 700
 };
 
-function meetsStaffRequirements(availableStaff, requiredStaff){
-  if (availableStaff.length >= requiredStaff){
-  	return true;
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+  if (availableStaff.length >= requiredStaff) {
+    return true;
   } else {
     return false;
   }
 };
 
-export {availableAirplanes, flightRequirements, meetsStaffRequirements};
+export function meetsSpeedRangeRequirements(maxSpeed,minSpeed,requiredSpeedRange){
+  let range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true;
+  } else {
+    return false;
+  }
+};
