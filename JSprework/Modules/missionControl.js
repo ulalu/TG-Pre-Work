78b4c6a043569a03d27a447e0885b5e3a1b9f2c1
Ula MV <uvanderlaan@gmail.com>
2018-07-31 22:55:50 -0600

    Ex 8

--- JSprework/Modules/missionControl.js
+++ JSprework/Modules/missionControl.js
@@ -1,8 +1,15 @@
-import Airplane from './airplane';
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
 
 function displayFuelCapacity(){
-  Airplane.availableAirplanes.forEach(function(element){console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  availableAirplanes.forEach(function(element){console.log('Fuel Capacity of ' + element.name + ': ' + element.fuelCapacity);
+  });
+}
+
+function displayStaffStatus(){
+  availableAirplanes.forEach(function(element){
+    console.log(element.name + 'meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff) );
   });
 }
 
 displayFuelCapacity();
+displayStaffStatus();
