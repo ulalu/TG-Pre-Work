33c97ff70efd8bc68680807262fcd5b8e339b7f7
Ula MV <uvanderlaan@gmail.com>
2018-08-01 00:16:30 -0600

    Ex 5

--- JSprework/Requests/1/main.js
+++ JSprework/Requests/1/main.js
@@ -1,11 +1,34 @@
-const xhr = new XMLHttpRequest();
-xhr.responseType = 'json';
-xhr.onreadystatechange = () => {
-  if (xhr.readyState === XMLHttpRequest.DONE){
-    return xhr.response;
-  }
-};
-xhr.open('GET', url);
-xhr.send();
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy='
 
-const url = 'https://api-to-call.com/endpoint'
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+const getSuggestions = () => {
+	const wordQuery = inputField.value;
+  const endpoint = url + ' ' + queryParams + ' ' + wordQuery;
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+  renderResponse(xhr.response)
+		}
+  };
+  xhr.open('GET', endpoint);
+  xhr.send();
+}
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  };
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
