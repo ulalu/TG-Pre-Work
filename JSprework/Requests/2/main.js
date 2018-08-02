2c177a090e8bf4dc6bd87fe9df6982dd33b4c92d
Ula MV <uvanderlaan@gmail.com>
2018-08-02 17:15:27 -0600

    Ex 5

--- JSprework/Requests/2/main.js
+++ JSprework/Requests/2/main.js
@@ -1,6 +1,6 @@
 // Information to reach API
 const url = 'https://api.datamuse.com/words';
-const queryParams = '?sl='
+const queryParams = '?sl=';
 
 // Selects page elements
 const inputField = document.querySelector('#input');
@@ -10,18 +10,18 @@ const responseField = document.querySelector('#responseField');
 // AJAX function
 const getSuggestions = () => {
   const wordQuery = inputField.value;
-  const endpoint = `${url}${queryParams}${wordQuery}`
+  const endpoint = `${url}${queryParams}${wordQuery}`;
 
   fetch(endpoint).then(response => {
     if (response.ok) {
       return response.json();
     }
     throw new Error('Request failed!');
-  }, (networkError) => {
-    console.log(networkError.message);
-  }
-
-})
+  }, networkError => {
+    console.log(networkError.message)
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse)
+  })
 }
 
 // Clears previous results and display results to webpage
