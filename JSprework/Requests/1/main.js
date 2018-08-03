acdb707a7885bcb5dc0dd81d1ec3d305fe473b78
Ula MV <uvanderlaan@gmail.com>
2018-08-02 18:08:41 -0600

    Ex 10

--- JSprework/Requests/1/main.js
+++ JSprework/Requests/1/main.js
@@ -10,28 +10,31 @@ const responseField = document.querySelector('#responseField');
 // AJAX functions
 const shortenUrl = () => {
   const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-
-  const xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-  		renderResponse(xhr.response);
-		}
-  }
-  xhr.open('POST', url);
-  xhr.setRequestHeader('Content-type', 'application/json');
-	xhr.setRequestHeader('apikey', apiKey);
-  xhr.send(data);
+  const data = JSON.stringify({destination:urlToShorten})
+  fetch(url,{
+    method: 'POST',
+    headers: {
+    'Content-type': 'application/json',
+    'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!')
+  },(networkError) => {
+    console.log(networkError.message);
+  }).then(jsonResponse => {
+    renderResponse(jsonResponse);
+  });
 }
-
 
 // Clear page and call AJAX functions
 const displayShortUrl = (event) => {
   event.preventDefault();
   while(responseField.firstChild){
-    responseField.removeChild(responseField.firstChild);
+    responseField.removeChild(responseField.firstChild)
   }
   shortenUrl();
 }
