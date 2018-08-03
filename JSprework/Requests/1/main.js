b26fb529425aa26354a024405c9a4bd9a5f61058
Ula MV <uvanderlaan@gmail.com>
2018-08-02 18:36:26 -0600

    Ex 16

--- JSprework/Requests/1/main.js
+++ JSprework/Requests/1/main.js
@@ -1,16 +1,43 @@
-const getData = async () => {
-  try{
-    const response = await fetch('https://api-to-call.com/endpoint', {
+// information to reach API
+const apiKey = '1b201b0800e3470187f0c417a41a5bd0';
+const url = 'https://api.rebrandly.com/v1/links';
+
+// Some page elements
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+// AJAX functions
+const shortenUrl = async () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
       method: 'POST',
-      body: JSON.stringify({id: 200})
-    });
-    if (response.ok) {
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+				'apikey': apiKey
+      }
+    })
+    if(response.ok){
       const jsonResponse = await response.json();
-      return jsonResponse;
+      renderResponse(jsonResponse);
     }
-    throw new Error('Request failed!')
+    ;
   }
   catch(error){
     console.log(error);
-  }
+  };
 }
+
+// Clear page and call AJAX functions
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
