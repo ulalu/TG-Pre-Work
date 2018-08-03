cab871b911bf0f19c5ee71800b2e9737f194bada
Ula MV <uvanderlaan@gmail.com>
2018-08-02 18:18:49 -0600

    Ex 13

--- JSprework/Requests/1/main.js
+++ JSprework/Requests/1/main.js
@@ -1,13 +1,37 @@
-const getData = async() => {
+// Information to reach API
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_jja=';
+
+// Selecting page elements
+const inputField = document.querySelector('#input');
+const submit = document.querySelector('#submit');
+const responseField = document.querySelector('#responseField');
+
+// AJAX function
+// Code goes here
+const getSuggestions= async () => {
+  const wordQuery = inputField.value;
+  const endpoint = `${url}${queryParams}${wordQuery}`
   try {
-    const response = await fetch('https://api-to-call.com/endpoint');
+    const response = await fetch(endpoint);
     if (response.ok) {
       const jsonResponse = await response.json();
-      return jsonResponse;
+      renderResponse(jsonResponse);
     }
-    throw new Error('Request failed!')
   }
-  catch(error) {
+  catch(error){
     console.log(error);
   }
-};
+}
+
+
+// Clear previous results and display results to webpage
+const displaySuggestions = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild)
+  }
+  getSuggestions();
+}
+
+submit.addEventListener('click', displaySuggestions);
